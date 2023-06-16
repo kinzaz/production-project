e72ae95374deedd8dae5b76db8ff46080bab86cf
Vlad Vlasenko <kinzazdev579@gmail.com>
2023-06-16 15:07:04 +0300

    variables and theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import App from "./App";
 import React from "react";
 import ReactDom from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 const root = ReactDom.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
