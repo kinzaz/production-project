dffabc3a7d9efb92f0ada989c5de5a804bade659
Vlad Vlasenko <kinzazdev579@gmail.com>
2023-06-17 13:48:08 +0300

    sidebar, layout app

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import styles from "./Navbar.module.scss";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string;
@@ -11,7 +10,6 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(styles.navbar, {}, [className])}>
-      <ThemeSwitcher /> 
       <div className={styles.links}>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,31 @@
+import { DetailedHTMLProps, HTMLAttributes, useState } from "react";
+import styles from "./Sidebar.module.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+
+type SidebarProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>;
+
+export const Sidebar = ({ className }: SidebarProps) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToggle = () => {
+    setCollapsed((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={classNames(styles.Sidebar, { [styles.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <button onClick={onToggle}>toggle</button>
+      <div className={styles.switchers}>
+        <ThemeSwitcher />
+        {/* LangSwitcher */}
+      </div>
+    </div>
+  );
+};
