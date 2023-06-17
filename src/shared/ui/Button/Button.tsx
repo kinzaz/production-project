edf326cacb24d2fe4b37c113b9c885ca62af607e
Vlad Vlasenko <kinzazdev579@gmail.com>
2023-06-17 13:01:47 +0300

    Svg loader. File loader. Button UI kit

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,31 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import styles from "./Button.module.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+
+export enum ThemeButton {
+  CLEAR = "clear",
+}
+
+interface ButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  theme?: ThemeButton;
+}
+
+export const Button = ({
+  className,
+  children,
+  theme,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={classNames(styles.Button, {}, [className, styles[theme]])}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,24 @@
+import styles from "./ThemeSwitcher.module.scss";
+import { Theme, useTheme } from "app/providers/ThemeProvider";
+import { classNames } from "shared/lib/classNames/classNames";
+import LightIcon from "shared/assets/icons/theme-light.svg";
+import DarktIcon from "shared/assets/icons/theme-dark.svg";
+import { Button, ThemeButton } from "shared/ui/Button/Button";
+
+interface ThemeSwitcherProps {
+  className?: string;
+}
+
+export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button
+      theme={ThemeButton.CLEAR}
+      className={classNames(styles.ThemeSwitcher, {}, [className])}
+      onClick={toggleTheme}
+    >
+      {theme === Theme.DARK ? <DarktIcon /> : <LightIcon />}
+    </Button>
+  );
+};
