45db8ac862c421b6153cf4c39e855971f100bc44
Vlad Vlasenko <kinzazdev579@gmail.com>
2023-08-15 12:59:46 +0300

    bugfix

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.async.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.async.tsx
@@ -1,9 +1,5 @@
 import { lazy } from 'react';
 
 export const ArticleDetailsPageAsync = lazy(
-  () =>
-    new Promise((resolve) => {
-      // @ts-ignore
-      setTimeout(() => resolve(import('./ArticleDetailsPage')), 1500);
-    })
+  () => import('./ArticleDetailsPage')
 );
