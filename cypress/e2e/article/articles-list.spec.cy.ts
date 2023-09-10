2c19bd94f2d872a64a5ecaec0f9b26d9c2814323
Vlad Vlasenko <kinzazdev579@gmail.com>
2023-09-10 17:29:32 +0300

    mocks, stubs, interceptors in e2e testing

--- cypress/e2e/article/articles-list.spec.cy.ts
+++ cypress/e2e/article/articles-list.spec.cy.ts
@@ -7,4 +7,14 @@ describe('Пользователь заходит на страницу спис
     cy.getByTestId('ArticleList').should('exist');
     cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
   });
+  it('на стабах (фикстурах)', () => {
+    cy.intercept('GET', '**/articles?*', { fixture: 'articles.json' });
+    cy.getByTestId('ArticleList').should('exist');
+    cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+  });
+  it.skip('Пример заскипанного теста', () => {
+    cy.getByTestId('ArticleList').should('exist');
+    cy.getByTestId('ArticfdfdleList').should('exist');
+    cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+  });
 });
